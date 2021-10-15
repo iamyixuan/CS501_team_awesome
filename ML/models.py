4065794068689b7fdb239c3e48c50fcbedcca867
Yixuan Sun <yixuan@Yixuans-Mac-mini.local>
2021-10-15 16:33:55 -0400

    rnn

---
+++ ML/models.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+"""
+Models:
+    RNN: standard RNN
+        args:
+            input_size: input sequence length
+            hidden_size: number of hidden states
+            output_size: output lenght
+    RNN with attention:
+    Transformer:
+"""
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
