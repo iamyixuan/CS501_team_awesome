4c3b8805316517f88df95be716c97a4050f8edcd
Yixuan Sun <yixuan@MBP16inch2021.hsd1.in.comcast.net>
2021-10-27 23:21:07 -0400

    revised dataloader

--- ML/data.py
+++ ML/data.py
@@ -15,16 +15,21 @@ class WeatherDataset(Dataset):
         self.horizon = horizon
 
         cloud_cover_dir = path + "ProcessedDataCLOUD/"
-        self.cloud_df = self.get_files(cloud_cover_dir + "FIRST TYPE/")
-        self.cloud_df2 = self.get_files(cloud_cover_dir + "SECOND TYPE/")
-        self.cloud_df3 = self.get_files(cloud_cover_dir + "THIRD TYPE/")
+        self.cloud_df = self.get_files(cloud_cover_dir + "FIRST/")
+        self.cloud_df2 = self.get_files(cloud_cover_dir + "SECOND/")
+        self.cloud_df3 = self.get_files(cloud_cover_dir + "THIRD/")
 
-        prcp_dir = path + "ProcessedDataPTYPE/"
+        prcp_dir = path + "FinalPtype/"
         self.prcp = self.get_files(prcp_dir)
+
+        temp_dir = path + "FINALTEMP/TEMP/"
+        self.temp = self.get_files(temp_dir)
+
+        dew_pt_dir = path + "FINALTEMP/DEWPOINT/"
+        self.dew_pt = self.get_files(dew_pt_dir) 
 
     def get_files(self, file_dir):
         filenames = sorted(glob.glob(file_dir + "*.csv")[:self.file_span])
-        print(filenames)
         dfs = []
         for f in filenames:
             df_tmp = pd.read_csv(f).values[:, :10] # use the first 10 stations
@@ -59,14 +64,16 @@ class WeatherDataset(Dataset):
         cloud_1 = self.get_data(self.cloud_df)
         cloud_2 = self.get_data(self.cloud_df2)
         cloud_3 = self.get_data(self.cloud_df3)
-
+        temp = self.get_data(self.temp)
+        dew_pt = self.get_data(self.dew_pt)
         prcp = self.get_label(self.prcp)
         Tensor = torch.FloatTensor
         Long = torch.LongTensor
-        sample = {"cloud_1": Tensor(cloud_1[idx]), "cloud_2": Tensor(cloud_2[idx]), " cloud_3": Tensor(cloud_3[idx]), "precipitation": Long(prcp[idx])}
+        sample = {"cloud_1": Tensor(cloud_1[idx]), "cloud_2": Tensor(cloud_2[idx]), " cloud_3": Tensor(cloud_3[idx]), 
+        "temperature": temp, "dew_point": dew_pt, "precipitation": Long(prcp[idx])}
         return sample
         
 
 if __name__ == "__main__":
-    dat = WeatherDataset("../../Processed Data/", 3, 12, 5)
+    dat = WeatherDataset("../../Processed Data/", 10, 12, 5)
     print(dat[23])
---
+++ utils/check_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import glob
+import numpy as np
+
+
+cloud1_path = "../../Processed Data/ProcessedDataCLOUD/THIRD/"
+prcp_path = "../../Processed Data/FinalPtype/"
+
+c_files = sorted(glob.glob(cloud1_path + "*.csv"))
+p_files = sorted(glob.glob(prcp_path + "*.csv"))
+
+for c, p in zip(c_files, p_files):
+    print(c[-9:], " ", p[-9:])
+    c_df = pd.read_csv(c)
+    p_df = pd.read_csv(p)
+    print("cloud df shape", c_df.shape, end=" ")
+    print("prcp df shape", p_df.shape)
+    print()
+
+# print(pd.read_csv(p_files[0]).head())
+# print(pd.read_csv(p_files[9]).head())
