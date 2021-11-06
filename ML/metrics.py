7c3fd6ceab88efa02bbe86c24ff642dfb478f816
Parker Farese <46821764+pjfarese@users.noreply.github.com>
2021-11-06 16:30:16 -0400

    Update metrics.py

--- ML/metrics.py
+++ ML/metrics.py
@@ -9,25 +9,29 @@ from sklearn.metrics import RocCurveDisplay
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
-f = open("metrics_out.txt","a")
-pred_val=np.genfromtxt("results.txt", dtype=float64, encoding=None, delimiter=",")
-true_val=np.genfromtxt("y_vals.txt", dtype=float64, encoding=None, delimiter=",")
+def metrics():
+  f = open("metrics_out.txt","a")
+  pred_val=np.genfromtxt("results.txt", dtype='f')
+  true_val=np.genfromtxt("y_vals.txt", dtype='f')
 
-acc=balanced_accuracy_score(true_val, pred_val)
+  acc=balanced_accuracy_score(true_val, pred_val)
 
-f1=f1_score(true_val, pred_val, average=None)
+  f1=f1_score(true_val, pred_val, average=None)
 
-prec=average_precision_score(true_val, pred_val)
+  prec=average_precision_score(true_val, pred_val)
 
-roc=roc_auc_score(true_val, pred_val)
-display = RocCurveDisplay.from_predictions(true_val, pred_val)
-plt.plot(display)
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.show()
+  roc=roc_auc_score(true_val, pred_val)
+  display = RocCurveDisplay.from_predictions(true_val, pred_val)
+  plt.plot(display)
+  plt.xlabel('False Positive Rate')
+  plt.ylabel('True Positive Rate')
+  plt.title('ROC Curve')
+  plt.show()
 
-print('%.3f,%.3f,%.3f,%.3f' % (acc,f1,prec,roc))
-print('%.3f,%.3f,%.3f,%.3f' % (acc,f1,prec,roc), file=f)
+  print('%.3f,%.3f,%.3f,%.3f' % (acc,f1,prec,roc))
+  print('%.3f,%.3f,%.3f,%.3f' % (acc,f1,prec,roc), file=f)
 
-return
+  return
+
+if __name__=='__main__':
+  metrics()
